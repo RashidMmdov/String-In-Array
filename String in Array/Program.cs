95d56c076289d02d3ffa418b7c25d22ab1efa5fd
RashidMmdov <tu7lw2f82@code.edu.az>
2022-03-04 23:36:05 +0400

    String in Arrat task

---
+++ String in Array/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace String_in_Array
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            String[] world = { "Rashid", "Ali", "BaxisAli", "Bahadur", "Tural" };
+            for(int i = 0; i < world.Length; i++)
+            {
+                if(world[i].Length > 5)
+                {
+                    Console.WriteLine(world[i]);
+                }
+               
+            }
+        }
+    }
+}
